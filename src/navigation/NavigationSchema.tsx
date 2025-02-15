221dd7eb1771e4a2ffe47f5c57fcf07c685a0ae0
hadeer <hadeeressam21@gmail.com>
2025-02-15 02:11:17 +0200

    handle the navigation between screens, and create all project screens navigation and tabBar

--- src/navigation/NavigationSchema.tsx
+++ src/navigation/NavigationSchema.tsx
@@ -11,6 +11,10 @@ import ViewMoreScreen from "../presentation/view/screen/home/viewMore"
 import DetailsScreen from "../presentation/view/screen/home/details"
 import SplashScreen from "../presentation/view/screen/Auth/splash"
 import OnboardingScreen from "../presentation/view/screen/Auth/onboarding"
+import { Octicons, Foundation, AntDesign, Ionicons } from '@expo/vector-icons';
+import COLOR from "../../constant"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { Pressable, StyleSheet, TouchableOpacity, View } from "react-native"
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
 
@@ -18,8 +22,12 @@ const Tab = createBottomTabNavigator()
 //Favorite Stack
 const FavoriteNavigator = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="" component={FavoriteScreen} />
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen name="favorite_main" component={FavoriteScreen} />
         </Stack.Navigator>
     )
 }
@@ -27,8 +35,12 @@ const FavoriteNavigator = () => {
 //Profile Stack    <= Will not implemented first
 const ProfileNavigator = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="" component={ProfileScreen} />
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen name="profile_main" component={ProfileScreen} />
         </Stack.Navigator>
     )
 }
@@ -36,8 +48,12 @@ const ProfileNavigator = () => {
 // Search Stack
 const SearchNavigator = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="" component={SearchScreen} />
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Stack.Screen name="search_main" component={SearchScreen} />
         </Stack.Navigator>
     )
 }
@@ -46,7 +62,11 @@ const SearchNavigator = () => {
 //Home Stack
 const HomeNavigator = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+        >
             <Stack.Screen name="main" component={HomeScreen} />
             <Stack.Screen name="more" component={ViewMoreScreen} />
             <Stack.Screen name="details" component={DetailsScreen} />
@@ -56,18 +76,83 @@ const HomeNavigator = () => {
 
 //Tab will be parent
 const MainTab = () => {
+    const instent = useSafeAreaInsets()
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="home" component={HomeNavigator} />
-            <Tab.Screen name="favorite" component={FavoriteNavigator} />
-            <Tab.Screen name="search" component={SearchNavigator} />
-            <Tab.Screen name="user" component={ProfileNavigator} />
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: COLOR.primary_btn,
+                    borderRadius: 36,
+                    marginBottom: 16,
+                    height: 80 + instent.bottom,
+                },
+                tabBarIconStyle: {
+                    alignSelf: 'center',
+                    height: "100%",
+                },
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: COLOR.white,
+                tabBarInactiveTintColor: COLOR.white,
+                tabBarButton: (props) => <Pressable {...props} android_ripple={{ color: COLOR.transparent }} />
+            }}
+        >
+            <Tab.Screen
+                name="home"
+                component={HomeNavigator}
+                options={{
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <>
+                            {
+                                focused ?
+                                    <Foundation name="home" size={size} color={color} />
+                                    :
+                                    <Octicons name="home" size={size} color={color} />
+                            }
+
+                        </>
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="favorite"
+                component={FavoriteNavigator}
+                options={{
+                    tabBarIcon: ({ size, color, focused }) => (
+                        <AntDesign size={size} color={color} name={focused ? "heart" : "hearto"} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="search"
+                component={SearchNavigator}
+                options={{
+                    tabBarIcon: ({ size, color, focused }) => (
+                        <>
+                            {
+                                focused &&
+                                <View style={[styles.fillSearchIcon, { width: size - 8, height: size - 8 }]} />
+                            }
+                            <Octicons size={size} color={color} name={"search"} />
+                        </>
+
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="user"
+                component={ProfileNavigator}
+                options={{
+                    tabBarIcon: ({ size, color, focused }) => (
+                        <Ionicons size={size} color={color} name={focused ? "person" : "person-outline"} />
+                    )
+                }}
+            />
         </Tab.Navigator>
     )
 }
 
 const AuthStack = () => {
-
     return (
         <Stack.Navigator
             screenOptions={{
@@ -91,3 +176,12 @@ const NavigationController = () => {
 }
 
 export default NavigationController
+
+const styles = StyleSheet.create({
+    fillSearchIcon: {
+        top: 24,
+        borderRadius: 10,
+        backgroundColor: COLOR.white,
+        position: 'absolute',
+    }
+})
