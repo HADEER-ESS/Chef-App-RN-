cf6904fb87b6d55f9e8acd07e383070119fb3bbb
hadeer <hadeeressam21@gmail.com>
2025-02-18 23:46:27 +0200

    add the provider for store

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import NavigationController from './src/navigation/NavigationSchema';
+import { Provider } from 'react-redux';
+import store from './src/store/store';
 
 export default function App() {
   return (
-    <NavigationController />
+    <Provider store={store}>
+      <NavigationController />
+    </Provider>
   );
 }
 
