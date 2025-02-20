10bfae6f7e06545ddddec53b79ab0a8cd856d6c8
hadeer <hadeeressam21@gmail.com>
2025-02-20 23:05:24 +0200

    apply changes in slicers files for random-all and random-category reciepes

--- src/presentation/viewModel/RandomByCategory.ts
+++ src/presentation/viewModel/RandomByCategory.ts
@@ -1,46 +1,49 @@
-import { createAsyncThunk, createSlice, SerializedError } from "@reduxjs/toolkit";
-import { RecipeByCategory } from "../../domain/model/category/categoryItem";
+// import { createAsyncThunk, createSlice, SerializedError } from "@reduxjs/toolkit";
+// import { RecipeByCategory } from "../../domain/model/category/categoryItem";
 import { GetRandomRecipeByCategoryUseCase } from "../../domain/useCase/randomCategory/RandomCategoryUseCase";
+import { useQuery } from "react-query";
 
 
 
-export const getRecipeByCategory = createAsyncThunk<RecipeByCategory[], string>("recipe/category", async (name: string) => {
-    return await GetRandomRecipeByCategoryUseCase(name)
-})
+// export const getRecipeByCategory = createAsyncThunk<RecipeByCategory[], string>("recipe/category", async (name: string) => {
+//     return await GetRandomRecipeByCategoryUseCase(name)
+// })
+
+export const useRecipeByCategory = (category: string) => useQuery(["categoryRecipes", category], () => GetRandomRecipeByCategoryUseCase(category))
 
 
-interface RandomCategoryState {
-    isLoading: boolean,
-    error: string | null,
-    data: RecipeByCategory[]
-}
+// interface RandomCategoryState {
+//     isLoading: boolean,
+//     error: string | null,
+//     data: RecipeByCategory[]
+// }
 
 
-const initialState: RandomCategoryState = {
-    isLoading: false,
-    error: null,
-    data: []
-}
+// const initialState: RandomCategoryState = {
+//     isLoading: false,
+//     error: null,
+//     data: []
+// }
 
-const RandomByCategorySlicer = createSlice({
-    name: "random_recipe_by_category",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(getRecipeByCategory.pending, (state) => {
-                state.isLoading = true
-            })
-            .addCase(getRecipeByCategory.fulfilled, (state, actions) => {
-                console.log("action ", actions.payload)
-                state.isLoading = false,
-                    state.data = actions.payload
-            })
-            .addCase(getRecipeByCategory.rejected, (state, action) => {
-                state.isLoading = false,
-                    state.error = action.error.message || "Something went wrong"
-            })
-    }
-})
+// const RandomByCategorySlicer = createSlice({
+//     name: "random_recipe_by_category",
+//     initialState,
+//     reducers: {},
+//     extraReducers: (builder) => {
+//         builder
+//             .addCase(getRecipeByCategory.pending, (state) => {
+//                 state.isLoading = true
+//             })
+//             .addCase(getRecipeByCategory.fulfilled, (state, actions) => {
+//                 console.log("action ", actions.payload)
+//                 state.isLoading = false,
+//                     state.data = actions.payload
+//             })
+//             .addCase(getRecipeByCategory.rejected, (state, action) => {
+//                 state.isLoading = false,
+//                     state.error = action.error.message || "Something went wrong"
+//             })
+//     }
+// })
 
-export default RandomByCategorySlicer.reducer
+// export default RandomByCategorySlicer.reducer
