ee72821dedc420bff4df2f2255fbeb24cef9e18c
hadeer <hadeeressam21@gmail.com>
2025-03-01 21:02:28 +0200

    handle the style for selected category with api call based on category

--- src/presentation/view/components/CategoryCard.tsx
+++ src/presentation/view/components/CategoryCard.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
 import COLOR from '../../../../constant'
+import HomeController from '../../viewController/appController/homeController'
 
 type props = {
     name: string,
-    isFocuse: boolean
+    selected: string,
     onclick: () => void
 }
 
-const CategoryCard = ({ name, isFocuse, onclick }: props) => {
+const CategoryCard = ({ name, selected, onclick }: props) => {
+    console.log("selected category now is ", selected);
+
     return (
-        <View style={[styles.cardComponent, {}]}>
-            <TouchableWithoutFeedback >
-                <Text style={[styles.textStyle, {}]}>{name}</Text>
+        <View style={[styles.cardComponent, { backgroundColor: selected === name ? COLOR.primary_btn : COLOR.white }]}>
+            <TouchableWithoutFeedback onPress={onclick}>
+                <Text style={[styles.textStyle, { color: selected === name ? COLOR.white : COLOR.primary_btn }]}>{name}</Text>
             </TouchableWithoutFeedback>
         </View>
 
