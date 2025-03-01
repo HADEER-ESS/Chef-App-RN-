4786e336803f06e75a5967906a63649ec5210804
hadeer <hadeeressam21@gmail.com>
2025-03-01 21:27:50 +0200

    create navigation to view-more screen with category to get data

--- src/presentation/view/screen/home/viewMore.tsx
+++ src/presentation/view/screen/home/viewMore.tsx
@@ -1,8 +1,17 @@
+import { RouteProp, useRoute } from '@react-navigation/native'
 import React from 'react'
+import { RootStackParamList } from '../../../../navigation/NavigationType'
+import { Text } from 'react-native'
+
+type MoreScreenRouteProps = RouteProp<RootStackParamList, "more">
 
 const ViewMoreScreen = () => {
+    const route = useRoute<MoreScreenRouteProps>()
+    const { category } = route.params
     return (
-        <></>
+        <Text>
+            {category}
+        </Text>
     )
 }
 
