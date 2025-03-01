a433ba61aaecf202b0308c4d2d0451468dbe12d4
hadeer <hadeeressam21@gmail.com>
2025-03-02 00:24:18 +0200

    add some style for viewMore screen

--- src/presentation/view/screen/home/viewMore.tsx
+++ src/presentation/view/screen/home/viewMore.tsx
@@ -1,18 +1,35 @@
 import { RouteProp, useRoute } from '@react-navigation/native'
 import React from 'react'
 import { RootStackParamList } from '../../../../navigation/NavigationType'
-import { Text } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import COLOR from '../../../../../constant'
+import CardComponent from '../../components/CardComponent'
+import { RecipeItemModule } from '../../../../domain/model/recipes/recipeItemModule'
+import ViewMoreController from '../../../viewController/appController/ViewMoreController'
 
-type MoreScreenRouteProps = RouteProp<RootStackParamList, "more">
 
 const ViewMoreScreen = () => {
-    const route = useRoute<MoreScreenRouteProps>()
-    const { category } = route.params
+    const { recipesData, category } = ViewMoreController()
     return (
-        <Text>
-            {category}
-        </Text>
+        <View style={styles.viewScreenStyle}>
+            <FlatList
+                data={[]}
+                keyExtractor={(item, index) => `${item}:${index}`}
+                renderItem={({ item, index }) => <CardComponent index={index} data={item as RecipeItemModule} addFav={() => { }} />}
+            />
+            <Text>
+                {category}
+            </Text>
+        </View>
+
     )
 }
 
 export default ViewMoreScreen
+
+const styles = StyleSheet.create({
+    viewScreenStyle: {
+        backgroundColor: COLOR.white,
+        flex: 1
+    }
+})
